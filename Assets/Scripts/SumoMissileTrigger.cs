2f97707c13b2bd58dd37d5aa083a9e304fb7ce6a
Hatemsla <kalashnikovjan@yandex.ru>
2022-12-25 12:15:05 +0300

    Fix sumo shooting

--- Assets/Scripts/SumoMissileTrigger.cs
+++ Assets/Scripts/SumoMissileTrigger.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class SumoMissileTrigger : MonoBehaviour
 {
-    private float _pushPower = 1000000f;
+    private float _pushPower = 500000f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Bot")
         {
-            other.gameObject.GetComponentInParent<SumoTankAI>().Push(transform.position, _pushPower);
+            other.gameObject.GetComponentInParent<SumoTankAI>().Push(other.transform.position, _pushPower);
         }
 
         if (other.gameObject.tag == "Player")
         {
-            other.gameObject.GetComponentInParent<SumoTankController>().Push(transform.position, _pushPower);
+            other.gameObject.GetComponentInParent<SumoTankController>().Push(other.transform.position, _pushPower);
         }
 
         if (other.gameObject.tag != "Checkpoint")
